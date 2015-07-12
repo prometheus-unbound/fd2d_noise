0eddd6a8435d1403966ce90c067af5fdded181e4
sagerk <korbinian.sager@erdw.ethz.ch>
2015-07-12 23:37:27 +0200

    type of source_dist changed for compilation to mex files

--- code/mex_functions/compile_data.m
+++ code/mex_functions/compile_data.m
@@ -13,12 +13,15 @@ i3 = coder.typeof(zeros(2,2),[inf,2],1);
 % integer switch
 i4 = coder.typeof(1,1,0);
 
+% source dist
+i5 = coder.typeof(zeros(2,2,2),[inf,inf,inf],1);
+
 
 % run_forward_green_fast(mu,src)
 codegen run_forward_green_fast.m -args {i2,i3}
 
 % run_forward_correlation_fast(G_2, source_dist, mu, rec)
-codegen run_forward_correlation_fast.m -args {i1,i2,i2,i3,i4}
+codegen run_forward_correlation_fast.m -args {i1,i5,i2,i3,i4}
 
 
 rmpath(genpath('../../'))
