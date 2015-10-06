df5e824f53eb4069c65309306bb588e79efa1598
sagerk <korbinian.sager@erdw.ethz.ch>
2015-10-06 15:31:20 +0200

    introduce variable for sampling frequency

--- input/input_interferometry.m
+++ input/input_interferometry.m
@@ -1,5 +1,5 @@
 
-function [f_sample,n_sample,w_sample,dw] = input_interferometry()
+function [f_sample,n_sample,w_sample,dw,nt_freq] = input_interferometry()
 
 %==========================================================================
 % input parameters for interferometry
@@ -18,3 +18,4 @@ f_sample=0.02:0.002:0.2;
 n_sample = length(f_sample);
 w_sample = 2*pi*f_sample;
 dw = w_sample(2) - w_sample(1);
+nt_freq = 5;
