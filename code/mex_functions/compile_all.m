af9092e18676475a71319dac91b3f55e5811b496
sagerk <korbinian.sager@erdw.ethz.ch>
2015-10-06 15:28:06 +0200

    adapt mex generation to new features

--- code/mex_functions/compile_all.m
+++ code/mex_functions/compile_all.m
@@ -2,10 +2,10 @@
 addpath(genpath('../../'))
 
 
-% G_2, C_2_dxv, C_2_dzv
+% G_2, C_2_dxv, C_2_dzv, df
 i1 = coder.typeof(complex(zeros(2,2,2)),[inf,inf,inf],1);
 
-% mu, adstf, spectrum
+% mu, ad-stf, spectrum
 i2 = coder.typeof(zeros(2,2),[inf,inf],1);
 
 % src, rec
@@ -21,11 +21,11 @@ i5 = coder.typeof(zeros(2,2,2),[inf,inf,inf],1);
 % run_forward_green_fast(mu,src)
 codegen run_forward_green_fast.m -args {i2,i3}
 
-% run_forward_correlation_fast(G_2, source_dist, spectrum, mu, rec)
-codegen run_forward_correlation_fast.m -args {i1,i5,i2,i2,i3,i4}
+% run_forward_correlation_fast(G_2, source_dist, noise_spectrum, mu, rec, mode, df)
+codegen run_forward_correlation_fast.m -args {i1,i5,i2,i2,i3,i4,i1}
 
-% run_noise_source_kernel_fast(G_2,mu,stf,adsrc)
-codegen run_noise_source_kernel_fast.m -args {i1,i2,i2,i3}
+% run_noise_source_kernel_fast(G_2, mu,spectrum, stf, adsrc)
+codegen run_noise_source_kernel_fast.m -args {i1,i2,i2,i2,i3}
 
 % run_noise_mu_kernel_fast(C_2_dxv, C_2_dzv, mu, stf, adsrc)
 codegen run_noise_mu_kernel_fast.m -args {i1,i1,i2,i2,i3}
--- code/mex_functions/compile_data.m
+++ code/mex_functions/compile_data.m
@@ -2,10 +2,10 @@
 addpath(genpath('../../'))
 
 
-% G_2, C_2, C_2_dxv, C_2_dzv
+% G_2, C_2_dxv, C_2_dzv, df
 i1 = coder.typeof(complex(zeros(2,2,2)),[inf,inf,inf],1);
 
-% mu, adstf, spectrum
+% mu, ad-stf, spectrum
 i2 = coder.typeof(zeros(2,2),[inf,inf],1);
 
 % src, rec
@@ -14,14 +14,16 @@ i3 = coder.typeof(zeros(2,2),[inf,2],1);
 % integer switch
 i4 = coder.typeof(1,1,0);
 
-% source dist
+% source_dist
 i5 = coder.typeof(zeros(2,2,2),[inf,inf,inf],1);
+
 
 % run_forward_green_fast(mu,src)
 codegen run_forward_green_fast.m -args {i2,i3}
 
-% run_forward_correlation_fast(G_2, source_dist, spectrum, mu, rec, mode)
-codegen run_forward_correlation_fast.m -args {i1,i5,i2,i2,i3,i4}
+% run_forward_correlation_fast(G_2, source_dist, noise_spectrum, mu, rec, mode, df)
+codegen run_forward_correlation_fast.m -args {i1,i5,i2,i2,i3,i4,i1}
+
 
 
 rmpath(genpath('../../'))
