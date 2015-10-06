82c6f268aa65ab170d1551c89bd9ba3463d10a7b
sagerk <korbinian.sager@erdw.ethz.ch>
2015-10-06 16:18:29 +0200

    update comments

--- inversion/optlib_callback_fct/new_iteration.m
+++ inversion/optlib_callback_fct/new_iteration.m
@@ -12,14 +12,20 @@ function [usr_par] = new_iteration( it, model, usr_par)
 % usr_par : auxiliary user defined parameters (optional)
 
 
+%- compute ratio of gradients ---------------------------------------------
 if(it>0)
     model.normg_frac = norm(model.gradient) / usr_par.model(1).normg;
 else
     model.normg_frac = 1.0;
 end
 
+
+%- some output - a bit puristic at the moment -----------------------------
 fprintf('\n%i\t misfit = %e\t norm(g)/norm(g0)=%e\n', it, model.objective, model.normg_frac)
 
+
+%- save model structure in usr_par strucuture -----------------------------
 usr_par.model(it+1) = orderfields( model );
+
 
 end
